94d486f125c558957e9ec5b9ed5ae8d6f77aad32
Matthew Grant <amattwgrant@gmail.com>
2020-10-21 11:05:15 -0400

    server running properly, no display yet

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.use('/', (req, res, next) => {
-	console.log('hi');
-})
+const mainRoutes = require('./routes');
+
+// app.use('/', (req, res, next) => {
+// 	console.log('hi');
+// 	next();
+// });
+
+app.listen(3000, () => {
+	console.log('This working?');
+});
