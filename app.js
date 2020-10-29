890d5f80fd5aa09099b8ad24826d620487a57b3a
Matthew Grant <amattwgrant@gmail.com>
2020-10-29 18:24:23 -0400

    reoved unneeded code

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 /*
  * Required necessary modules and middleware
  */
-
 const express = require('express');
 const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
-
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
